9eecdb4f95a5642e6fa48d2f50a979c3bb29843c
Jacqui Lough <lough.jacqui@gmail.com>
2020-06-24 09:29:03 +1000

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <section className="section">
+      <div className="container">
+        <h1 className="title">tinynews demo</h1>
+        <p className="subtitle">
+          This site will be built using webiny and styled with <strong>Bulma</strong>!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      </div>
+    </section>
   );
 }
 
