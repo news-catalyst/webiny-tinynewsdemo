170c2259de1a4c763b24b6a3752bf6111a542351
Jacqui Lough <lough.jacqui@gmail.com>
2020-06-29 10:27:54 +1000

    Added mutation that updates an existing article

--- src/graphql/mutations.js
+++ src/graphql/mutations.js
@@ -33,3 +33,43 @@ export const CREATE_ARTICLE = gql `
       }
     }
   }`
+
+export const UPDATE_ARTICLE = gql`
+mutation UpdateBasicArticle($id: ID!, $data: BasicArticleInput!) {
+  content: updateBasicArticle(where: { id: $id }, data: $data) {
+    data {
+      id
+      headline {
+        values {
+          value
+          locale
+        }
+      }
+      body {
+        values {
+          value
+          locale
+        }
+      }
+      byline {
+        values {
+          value {
+            id
+            meta {
+              title {
+                value
+              }
+            }
+          }
+          locale
+        }
+      }
+      savedOn
+    }
+    error {
+      message
+      code
+      data
+    }
+  }
+}`
